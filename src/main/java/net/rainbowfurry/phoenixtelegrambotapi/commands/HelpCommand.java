e00a9332cdcdb645d942a720072fa41faf38cc1a
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-04 18:35:25 +0100

    Added Help Command with all Commands

---
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/HelpCommand.java
@@ -0,0 +1,25 @@
+package net.rainbowfurry.phoenixtelegrambotapi.commands;
+
+import net.rainbowfurry.phoenixtelegrambotapi.TextMessage;
+import net.rainbowfurry.phoenixtelegrambotapi.TelegramBot;
+
+import java.util.Map;
+
+public class HelpCommand implements Command{
+    @Override
+    public String getCommandDescription() {
+        return "Get a List with all valid Commands";
+    }
+
+    @Override
+    public void command(String channelID, String message) {
+
+        String _message = "A Help List with all valid Bot Commands:\n\n";
+
+        for(Map.Entry<String, Command> entry : TelegramBot.commands.entrySet()){
+            _message += "/" + entry.getKey() + " - " + entry.getValue().getCommandDescription() + "\n";
+        }
+
+        TextMessage.sendMessage(channelID, _message);
+    }
+}
