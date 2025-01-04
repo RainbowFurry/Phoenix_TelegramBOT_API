eacf5e11293ebfba4c92b440a83b8a5213338128
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-04 18:37:55 +0100

    Test

--- src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/TestCommand.java
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/TestCommand.java
@@ -1,11 +1,39 @@
 package net.rainbowfurry.phoenixtelegrambotapi.commands;
 
-import net.rainbowfurry.phoenixtelegrambotapi.BotMessage;
+import net.rainbowfurry.phoenixtelegrambotapi.ButtonMessage;
+import net.rainbowfurry.phoenixtelegrambotapi.TextMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public class TestCommand implements Command{
 
     @Override
+    public String getCommandDescription() {
+        return "Test";
+    }
+
+    @Override
     public void command(String channelID, String message) {
-        BotMessage.sendMessage(channelID, "TEST TEST TEST");
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(channelID);
+        sendMessage.setText("TEST");
+
+        ButtonMessage buttonMessage = new ButtonMessage();
+        buttonMessage.createRow();
+        buttonMessage.addButton(buttonMessage.createButton("Option 1", "option1"));
+        buttonMessage.addButton(buttonMessage.createButton("Option 2", "option2"));
+        buttonMessage.completeRow();
+        buttonMessage.createRow();
+        buttonMessage.addButton(buttonMessage.createButton("Test", "test"));
+        buttonMessage.completeRow();
+        buttonMessage.createRow();
+        buttonMessage.addButton(buttonMessage.createButton("Test 1", "test1"));
+        buttonMessage.addButton(buttonMessage.createButton("Test 2", "test2"));
+        buttonMessage.completeRow();
+
+        sendMessage.setReplyMarkup(buttonMessage.build());
+
+        TextMessage.sendMessage(sendMessage);
+        TextMessage.sendMessage(channelID, "TEST TEST TEST");
     }
 }
