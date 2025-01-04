3a49d9904d1937b953bfaa34de4a752b3a9e6076
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-04 18:36:09 +0100

    Added Command Info

--- src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/Command.java
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/Command.java
@@ -2,6 +2,8 @@ package net.rainbowfurry.phoenixtelegrambotapi.commands;
 
 public interface Command {
 
+    String getCommandDescription();
+
     void command(String channelID, String message);
 
 }
