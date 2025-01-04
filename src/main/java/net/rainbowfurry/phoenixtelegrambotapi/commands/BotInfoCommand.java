dd2fe39b83b7ee7fb07fed54c24d30fd9f268a1e
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-04 18:35:01 +0100

    Added Bot Info Command

---
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/commands/BotInfoCommand.java
@@ -0,0 +1,24 @@
+package net.rainbowfurry.phoenixtelegrambotapi.commands;
+
+import net.rainbowfurry.phoenixtelegrambotapi.TextMessage;
+import net.rainbowfurry.phoenixtelegrambotapi.TextFormatter;
+
+public class BotInfoCommand implements Command{
+    @Override
+    public String getCommandDescription() {
+        return "Shows Info about the Bot";
+    }
+
+    @Override
+    public void command(String channelID, String message) {
+
+        String _message = "The Phoenix Telegram Bot is made by @RainbowFurry and running with Java.\n" +
+                "It is made with oure own API using the Telegram API\n" +
+                "https://docs.rainbowfurry.com/phoenix-telegram-bot-api" +
+                TextFormatter.codePre("TelegramBot telegramBot = new TelegramBot(BOT_TOKEN, BOT_NAME);", "java");
+
+        TextMessage.sendMessage(channelID, message);
+        TextMessage.sendMessage(channelID, TextFormatter.codePre("TelegramBot telegramBot = new TelegramBot(BOT_TOKEN, BOT_NAME);", "java"));
+
+    }
+}
