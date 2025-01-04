cff4b2b535e84b6b75bbd3d4564042b4c67fc57e
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2025-01-04 18:36:32 +0100

    Listener Update - added Listener

--- src/main/java/net/rainbowfurry/phoenixtelegrambotapi/events/core/EventListener.java
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/events/core/EventListener.java
@@ -1,34 +1,50 @@
 package net.rainbowfurry.phoenixtelegrambotapi.events.core;
 
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.*;
 
 public interface EventListener {
 
     default void onUpdate(Update update){}
-    default void onCommand(String command){}
+    default void onChannelUpdate(Update update){}
+    default void onCallback(CallbackQuery callbackQuery){}
+    default void onChannelCallback(CallbackQuery callbackQuery){}
+    default void onCommand(Message command){}
+    default void onChannelCommand(Message message){}
     default void onMessage(Message message){}
     default void onChannelMessage(Message message){}
 
-    default void onAudioMessage(){}
-    default void onLocationMessage(){}
-    default void onContactMessage(){}
-    default void onDocumentMessage(){}
-    default void onStickerMessage(){}
-    default void onPhotoMessage(){}
-    default void onVideoMessage(){}
-    default void onVoiceMessage(){}
+    default void onAudioMessage(Message message){}
+    default void onChannelAudioMessage(Message message){}
+    default void onLocationMessage(Message message){}
+    default void onChannelLocationMessage(Message message){}
+    default void onContactMessage(Message message){}
+    default void onChannelContactMessage(Message message){}
+    default void onDocumentMessage(Message message){}
+    default void onChannelDocumentMessage(Message message){}
+    default void onStickerMessage(Message message){}
+    default void onChannelStickerMessage(Message message){}
+    default void onPhotoMessage(Message message){}
+    default void onChannelPhotoMessage(Message message){}
+    default void onVideoMessage(Message message){}
+    default void onChannelVideoMessage(Message message){}
+    default void onVoiceMessage(Message message){}
+    default void onChannelVoiceMessage(Message message){}
 
-    default void onMessageEdit(){}
-    default void onMessageReply(){}
-    default void onMessageShareForward(){}
-    default void onMessageDelete(){}
-    default void onMessagePin(){}
+    default void onMessageEdit(Message message){}
+    default void onChannelMessageEdit(Message message){}
+    default void onMessageReply(Message message){}
+    default void onChannelMessageReply(Message message){}
+    default void onMessageShareForward(Message message){}
+    default void onChannelMessageShareForward(Message message){}
+    default void onMessageDelete(Message message){}
+    default void onChannelMessageDelete(Message message){}
+    default void onMessagePin(Message message){}
+    default void onChannelMessagePin(Message message){}
     //default void onMessageReact(){}//nicht möglich?
 
-    default void onUserJoin(String chatId){}
-    default void onUserLeave(String chatId){}
-    default void onBotJoinChannel(){}
-    default void onBotLeaveChannel(){}
+    default void onJoin(User user, Chat chat){}
+    default void onChannelJoin(User user, Chat chat){}
+    default void onLeave(User user, Chat chat){}
+    default void onChannelLeave(User user, Chat chat){}
 
 }
--- src/main/java/net/rainbowfurry/phoenixtelegrambotapi/events/core/EventManager.java
+++ src/main/java/net/rainbowfurry/phoenixtelegrambotapi/events/core/EventManager.java
@@ -1,5 +1,7 @@
 package net.rainbowfurry.phoenixtelegrambotapi.events.core;
 
+import org.telegram.telegrambots.meta.api.objects.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,22 +13,187 @@ public class EventManager {
         listeners.add(listener);
     }
 
-    private void executeEvents(String chatId) {
+
+    public void onUpdate(Update update){
         for (EventListener listener : listeners) {
-            listener.onUserJoin(chatId);
-            listener.onUserLeave(chatId);
+            listener.onUpdate(update);
         }
     }
 
-    public void notifyUserJoin(String chatId) {
+    public void onChannelUpdate(Update update){
         for (EventListener listener : listeners) {
-            listener.onUserJoin(chatId);
+            listener.onChannelUpdate(update);
         }
     }
 
-    public void notifyUserLeave(String chatId) {
+    public void onCommand(Message message){
         for (EventListener listener : listeners) {
-            listener.onUserLeave(chatId);
+            listener.onCommand(message);
+        }
+    }
+
+    public void onChannelCommand(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelCommand(message);
+        }
+    }
+
+    public void onMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onMessage(message);
+        }
+    }
+
+    public void onChannelMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelMessage(message);
+        }
+    }
+
+
+    public void onAudioMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onAudioMessage(message);
+        }
+    }
+
+    public void onChannelAudioMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelAudioMessage(message);
+        }
+    }
+
+    public void onLocationMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onLocationMessage(message);
+        }
+    }
+
+    public void onChannelLocationMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelLocationMessage(message);
+        }
+    }
+
+    public void onContactMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onContactMessage(message);
+        }
+    }
+
+    public void onChannelContactMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelContactMessage(message);
+        }
+    }
+
+    public void onDocumentMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onDocumentMessage(message);
+        }
+    }
+
+    public void onChannelDocumentMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelDocumentMessage(message);
+        }
+    }
+
+    public void onStickerMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onStickerMessage(message);
+        }
+    }
+
+    public void onChannelStickerMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelStickerMessage(message);
+        }
+    }
+
+    public void onPhotoMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onPhotoMessage(message);
+        }
+    }
+
+    public void onChannelPhotoMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelPhotoMessage(message);
+        }
+    }
+
+    public void onVideoMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onVideoMessage(message);
+        }
+    }
+
+    public void onChannelVideoMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelVideoMessage(message);
+        }
+    }
+
+    public void onVoiceMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onVoiceMessage(message);
+        }
+    }
+
+    public void onChannelVoiceMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelVoiceMessage(message);
+        }
+    }
+
+
+    public void onEditMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onMessageEdit(message);
+        }
+    }
+
+    public void onChannelEditMessage(Message message){
+        for (EventListener listener : listeners) {
+            listener.onChannelMessageEdit(message);
+        }
+    }
+
+
+    public void onJoin(User user, Chat chat) {
+        for (EventListener listener : listeners) {
+            listener.onJoin(user, chat);
+        }
+    }
+
+    public void onChannelJoin(User user, Chat chat) {
+        for (EventListener listener : listeners) {
+            listener.onChannelJoin(user, chat);
+        }
+    }
+
+    public void onLeave(User user, Chat chat) {
+        for (EventListener listener : listeners) {
+            listener.onLeave(user, chat);
+        }
+    }
+
+    public void onChannelLeave(User user, Chat chat) {
+        for (EventListener listener : listeners) {
+            listener.onChannelLeave(user, chat);
+        }
+    }
+
+    public void onCallBack(CallbackQuery callbackQuery){
+        for (EventListener listener : listeners) {
+            listener.onCallback(callbackQuery);
+        }
+    }
+
+    public void onChannelCallBack(CallbackQuery callbackQuery){
+        for (EventListener listener : listeners) {
+            listener.onChannelCallback(callbackQuery);
         }
     }
 
